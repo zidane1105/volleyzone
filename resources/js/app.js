4c7973598e9aaaf633419804746a93c9a7beeebb
zidane1105 <hawarimifta@gmail.com>
2026-06-29 22:43:01 +0700

    fix: use npm package for ziggy to fix vercel build

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,7 +4,7 @@ import './bootstrap';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import { ZiggyVue } from 'ziggy-js';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
